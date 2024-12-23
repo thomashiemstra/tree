222186bdd4a47efda69a2f886e4e9f0462e0688a
Thomas <thomas-hiemstra@hotmail.com>
2024-12-23 18:55:52 +0100

    fourth commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.init()
 WIDTH, HEIGHT = 1920, 1080
 RES = (WIDTH, HEIGHT)
 
-FONT_SIZE = 30
+FONT_SIZE = 35
 alpha_value = randrange(30, 40, 5)
 
 chars = ['ｦ', 'ｱ', 'ｳ', 'ｴ', 'ｵ', 'ｶ', 'ｷ', 'ｹ', 'ｺ', 'ｻ', 'ｼ', 'ｽ', 'ｾ', 'ｿ', 'ﾀ', 'ﾂ', 'ﾃ', 'ﾅ', 'ﾆ', 'ﾇ', 'ﾈ',
@@ -23,75 +23,58 @@ green_chars_2 = [font_2.render(char, True, (0, 255, 0)) for char in chars]
 green_chars_3 = [font_3.render(char, True, (0, 255, 0)) for char in chars]
 green_chars_4 = [font_4.render(char, True, (0, 255, 0)) for char in chars]
 
+charsets = [green_chars, green_chars_2, green_chars_3, green_chars_4]
+
 screen = pygame.display.set_mode(RES)
 background = pygame.Surface(RES)
 display_surface = pygame.Surface(RES)
-display_surface.set_alpha(18)
+display_surface.set_alpha(22)
 # pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 clock = pygame.time.Clock()
 
 
 class Symbol:
-    def __init__(self, x, y):
+    def __init__(self, x, characters):
         self.x = x
-        self.y = y
-        self.speed = 30
-        self.value = choice(green_chars)
+        self.y = randrange(-HEIGHT, 0)
+        self.speed = 40
+        self.characters = characters
+        self.val = choice(self.characters)
+
+    def _draw(self):
+        if HEIGHT > self.y > 0 and background.get_at((self.x, self.y)) == (0, 0, 0, 255):
+            screen.blit(self.val, (self.x, self.y))
 
     def draw(self):
-        val = choice(green_chars)
-        self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE * randrange(1, 2)
-        if HEIGHT > self.y > 0 and background.get_at((self.x, self.y)) != (255, 255, 255, 255):
-            screen.blit(val, (self.x, self.y))
-
-    def draw_2(self):
-        val = choice(green_chars_2)
-        self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE * randrange(1, 2)
-        if HEIGHT > self.y > 0 and background.get_at((self.x, self.y)) != (255, 255, 255, 255):
-            screen.blit(val, (self.x, self.y))
-
-    def draw_3(self):
-        val = choice(green_chars_3)
-        self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE * randrange(1, 2)
-        if HEIGHT > self.y > 0 and background.get_at((self.x, self.y)) != (255, 255, 255, 255):
-            screen.blit(val, (self.x, self.y))
-
-    def draw_4(self):
-        val = choice(green_chars_4)
-        self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE * randrange(1, 2)
-        if HEIGHT > self.y > 0 and background.get_at((self.x, self.y)) != (255, 255, 255, 255):
-            screen.blit(val, (self.x, self.y))
+        # self._draw()
+        self.val = choice(green_chars)
+        self.y = self.y + self.speed if self.y < HEIGHT else -FONT_SIZE * randrange(1, 4)
+        self._draw()
 
 
-symbols = [Symbol(x, randrange(-HEIGHT, 0)) for x in range(0, WIDTH, FONT_SIZE * 4)]
-symbols_2 = [Symbol(x, randrange(-HEIGHT, 0)) for x in range(FONT_SIZE, WIDTH, FONT_SIZE * 4)]
-symbols_3 = [Symbol(x, randrange(-HEIGHT, 0)) for x in range(FONT_SIZE * 2, WIDTH, FONT_SIZE * 4)]
-symbols_4 = [Symbol(x, randrange(-HEIGHT, 0)) for x in range(FONT_SIZE * 3, WIDTH, FONT_SIZE * 4)]
+symbols_rows = []
+for counter, charset in enumerate(charsets):
+    symbols_rows.append([Symbol(x, charset) for x in range(FONT_SIZE * counter, WIDTH, FONT_SIZE * len(charsets))])
 
-run = True
-
-bg = pygame.image.load("tree2.png")
+bg = pygame.image.load("tree.png")
 bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
 rect = bg.get_rect()
 x_c, y_c = rect.center
 background.blit(bg, (WIDTH/2 - x_c, HEIGHT/2 - y_c))
 
+run = True
 while run:
 
     screen.blit(display_surface, (0, 0))
     display_surface.fill(pygame.Color('black'))
 
-    [symbol.draw() for symbol in symbols]
-    [symbol_2.draw_2() for symbol_2 in symbols_2]
-    [symbol_3.draw_3() for symbol_3 in symbols_3]
-    [symbol_4.draw_3() for symbol_4 in symbols_4]
+    for symbols in symbols_rows:
+        [symbol.draw() for symbol in symbols]
 
     pygame.time.delay(100)
-
     pygame.display.update()
-
-    clock.tick(60)
+    # clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
